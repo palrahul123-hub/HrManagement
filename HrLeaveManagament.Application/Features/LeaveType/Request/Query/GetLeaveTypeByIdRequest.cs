e2d4ece65791a242c10f32f88ee5a48c73141581
Rahul123 <pr@unstoppablesoftwares.com>
2024-12-18 07:41:59 +0530

    test

--- HrLeaveManagament.Application/Features/LeaveType/Request/Query/GetLeaveTypeByIdRequest.cs
+++ HrLeaveManagament.Application/Features/LeaveType/Request/Query/GetLeaveTypeByIdRequest.cs
@@ -6,5 +6,6 @@ namespace HrLeaveManagament.Application.Features.LeaveType.Request.Query
     public class GetLeaveTypeByIdRequest : IRequest<LeaveTypeDto>
     {
         public int Id { get; set; }
+        public string Name { get; set; }
     }
 }
